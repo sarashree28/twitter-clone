6198e0b14b136c95988086431748bd5befeffc1b
Sara Shree <sarashree22@gmail.com>
2024-08-10 22:16:44 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRoutes from './routes/user.route.js';
 import {v2 as cloudinary} from "cloudinary";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from './routes/notification.route.js';
+import path from "path";
 
 dotenv.config();
 console.log(process.env.MONGO_URI);
@@ -20,6 +21,7 @@ cloudinary.config({
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
@@ -30,6 +32,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
